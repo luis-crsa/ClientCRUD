1b16d55bf151b1d668d2440d8eaaf5c9b5e1f11e
Luis <luiscrsarmento@gmail.com>
2024-11-28 15:47:10 -0300

    Get Clients paged

--- src/main/java/com/project/clientcrud/services/ClientService.java
+++ src/main/java/com/project/clientcrud/services/ClientService.java
@@ -3,7 +3,10 @@ package com.project.clientcrud.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.project.clientcrud.dto.ClientDTO;
 import com.project.clientcrud.entities.Client;
@@ -15,9 +18,16 @@ public class ClientService {
     @Autowired
     ClientRepository repository;
 
+    @Transactional(readOnly = true)
     public ClientDTO findById(Long id){
         Optional<Client> result = repository.findById(id);
         return new ClientDTO(result.get());
     }
-    
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
+    }
+
 }
